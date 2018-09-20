16bd1f210344874002778290435ac1d6463e27fe
aegroot <alex.a.groot@student.hu.nl>
2018-09-20 11:10:41 +0200

    oefeningen

---
+++ les05/oefening5_4.py
@@ -0,0 +1,8 @@
+deeldoordrie = -0
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+for getal in lst:
+    if getal % 3 == 0:
+        # print getal
+        deeldoordrie = deeldoordrie + 1
+
+print deeldoordrie
