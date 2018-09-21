33735860924614c75e8b5589fbd5e8f44be603f2
aegroot <alex.a.groot@student.hu.nl>
2018-09-21 14:13:00 +0200

    oefeningen

---
+++ les06/oefening6_1.py
@@ -0,0 +1,7 @@
+getallenrij = [2, 4, 6, 10, 9, 7]
+max = -1
+for getal in getallenrij:
+    if getal > max:
+        max = getal
+
+print max
---
+++ les06/oefening6_2.py
@@ -0,0 +1,6 @@
+getallenrij = [2, 4, 6, 10, 9, 7]
+getal = int(input("kies een getal"))
+if getal in getallenrij:
+    print ("true")
+else:
+    print ("false")
