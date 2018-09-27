4e4abc16bb860e27953279428baf60310eb2a344
aegroot <alex.a.groot@student.hu.nl>
2018-09-27 23:16:44 +0200

    oefeningen

---
+++ opdrachten/1.functie_drie_parameters.py
@@ -0,0 +1,11 @@
+getal1 = 4
+getal2 = 3
+getal3 = 5
+
+
+def som(*args):
+    return sum(args)
+
+
+som()
+print(som(getal1, getal2, getal3))
--- opdrachten/5.functie_met_list_parameter_for-loops.py
+++ opdrachten/5.functie_met_list_parameter_for-loops.py
@@ -1,10 +1,9 @@
+
 def kwadraten_som(lst) -> object:
     getal = 0
     for getal in lst:
         if getal > 0:
             getal = getal ** 2
             print(getal)
-
-
 lst = [4, 5, 3, -81]
 kwadraten_som(lst)
