57c8e58e6480b36c6d39dfe53fe088f2f4518ef1
aegroot <alex.a.groot@student.hu.nl>
2018-09-27 09:49:13 +0200

    oefeningen

--- opdrachten/5.functie_met_list_parameter_for-loops.py
+++ opdrachten/5.functie_met_list_parameter_for-loops.py
@@ -1,11 +1,10 @@
-def kwadraten_som() -> object:
+def kwadraten_som(lst) -> object:
     getal = 0
     for getal in lst:
         if getal > 0:
             getal = getal ** 2
             print(getal)
-    return kwadraten_som()
 
 
 lst = [4, 5, 3, -81]
-kwadraten_som()
+kwadraten_som(lst)
