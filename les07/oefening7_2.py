ccb38db7a1faa4758851c4db8da79dbb63ebe00f
aegroot <alex.a.groot@student.hu.nl>
2018-09-27 10:58:26 +0200

    hello

---
+++ les07/oefening7_2.py
@@ -0,0 +1,17 @@
+s = '   voorbeeld \n'
+print(s.capitalize())
+print (s)
+print(s.count('e'))
+print(s.find('e'))
+print(s.replace('e','a'))
+
+s ='VoOrBeElD'
+print(s.lower())
+print(s.upper())
+print(s.strip())
+
+s= 'dit is een voorbeeld'
+print(s.split())
+s= 'dit-is-een-voorbeeld'
+print(s.split())
+print(s.split("-"))
